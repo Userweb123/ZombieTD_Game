be35c2773250bed7751eff34cb993d80bc124fbf
Sdeall <matviy.boshko@gmail.com>
2026-02-02 10:12:18 +0200

    Налаштував робочий простір юніті під подальшу розробку і налаштував систему білду

--- Assets/Scripts/PlayerHPSystem.cs
+++ Assets/Scripts/PlayerHPSystem.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using System;
 
 public class PlayerHPSystem : MonoBehaviour
 {
@@ -8,20 +7,35 @@ public class PlayerHPSystem : MonoBehaviour
     int hp;
     public void ModifyHP(int value)
     {
-        hp+=value;
-        hp=Mathf.Clamp(hp, 0, maxHP);
+        hp += value;
+        hp = Mathf.Clamp(hp, 0, maxHP);
+
+        if (hp==0)
+        {
+            Die();
+        }
+
+        Debug.Log("Hp modified by: " + value + ", current hp: " + hp);
     }
 
     void Start()
     {
-        hp=maxHP;
+        hp = maxHP;
     }
 
     void Update()
     {
+        //Видалити цей код пізніше!!!
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ModifyHP(-1);
         }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player Died");
+
+        //Це момент коли ти помираєш, чувак
     }
 }
